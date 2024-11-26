039b87eb06a010310f54d089aa27394854db032f
Sihun23 <gcsw16@Gachonui-MacBookPro-2.local>
2024-11-26 15:00:23 +0900

    :hammer:fix:명세서 버전 수정

--- src/main/java/myaong/popolog/psservice/controller/PsController.java
+++ src/main/java/myaong/popolog/psservice/controller/PsController.java
@@ -21,7 +21,7 @@ public class PsController {
 
     private final PsService psService;
 
-    @Operation(summary = "API 명세서 v0.5 line 66", description = "자기소개서 목록 조회")
+    @Operation(summary = "API 명세서 v0.4 line 66", description = "자기소개서 목록 조회")
     @GetMapping
     public ResponseEntity<ApiResponse<List<PsPreResponse>>> getPsList(
             @RequestHeader("memberId") Long memberId) {
@@ -29,7 +29,7 @@ public class PsController {
         return ResponseEntity.ok(ApiResponse.onSuccess(response));
     }
 
-    @Operation(summary = "API 명세서 v0.5 line 67", description = "자기소개서 상세조회")
+    @Operation(summary = "API 명세서 v0.4 line 67", description = "자기소개서 상세조회")
     @GetMapping("/{psId}")
     public ResponseEntity<ApiResponse<PsResponse>> getPs(
             @RequestHeader("memberId") Long memberId,
@@ -38,7 +38,7 @@ public class PsController {
         return ResponseEntity.ok(ApiResponse.onSuccess(response));
     }
 
-    @Operation(summary = "API 명세서 v0.5 line 68", description = "자기소개서 작성")
+    @Operation(summary = "API 명세서 v0.4 line 68", description = "자기소개서 작성")
     @PostMapping
     public ResponseEntity<ApiResponse<PsIdResponse>> createPs(
             @RequestHeader("memberId") Long memberId,
@@ -47,7 +47,7 @@ public class PsController {
         return ResponseEntity.ok(ApiResponse.onSuccess(response));
     }
 
-    @Operation(summary = "API 명세서 v0.5 line 69", description = "자기소개서 수정")
+    @Operation(summary = "API 명세서 v0.4 line 69", description = "자기소개서 수정")
     @PutMapping("/{psId}")
     public ResponseEntity<ApiResponse<Object>> updatePs(
             @RequestHeader("memberId") Long memberId,
@@ -57,7 +57,7 @@ public class PsController {
         return ResponseEntity.ok(ApiResponse.onSuccess(null));
     }
 
-    @Operation(summary = "API 명세서 v0.5 line 70", description = "자기소개서 목록 삭제")
+    @Operation(summary = "API 명세서 v0.4 line 70", description = "자기소개서 목록 삭제")
     @DeleteMapping("/{psId}")
     public ResponseEntity<ApiResponse<Object>> deletePs(
             @RequestHeader("memberId") Long memberId,
@@ -66,7 +66,7 @@ public class PsController {
         return ResponseEntity.ok(ApiResponse.onSuccess(null));
     }
 
-    @Operation(summary = "API 명세서 v0.5 line 71", description = "자기소개서 목록 첨삭")
+    @Operation(summary = "API 명세서 v0.4 line 71", description = "자기소개서 목록 첨삭")
     @GetMapping("/{psId}/editing")
     public ResponseEntity<ApiResponse<PsResponse>> editPs(
             @RequestHeader("memberId") Long memberId,
